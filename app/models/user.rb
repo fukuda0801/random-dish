329205d5536938acf3684d598a0421a2839a21a8
fukuda0801 <nagisano0801@gmail.com>
2024-02-01 09:49:19 +0900

    user: ゲストユーザー機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   validates :name, presence: true, length: { maximum: 20 }
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 },
-            format: { with: /\A[a-zA-Z0-9]+\z/, message: 'は半角英数字で入力してください' }
+  validates :email, uniqueness: true
+  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/, message: 'は半角英数字で入力してください' }
   validates :sex, presence: true
+
+  def self.guest
+    find_or_create_by!(email: 'guest1@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲストユーザー"
+      user.sex = "未設定(ゲスト用)"
+      user.confirmed_at = Time.now
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root to: 'homes#index'
   get 'users/show'
   devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   resources :homes, only: [:index, :show]
   resources :users, except: [:new, :create]
 end
